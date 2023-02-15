bbb3a81e599808aff939b21cb30a447aae355761
Hunters565 <adrianagustinust@gmail.com>
2023-02-15 17:42:29 +0700

    nvim config

--- nvim/lua/adr/keymap.lua
+++ nvim/lua/adr/keymap.lua
@@ -1,2 +1,8 @@
 vim.keymap.set("v" , "<C-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v" , "<C-k>", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-e>", ":NvimTreeToggle<cr>")
+vim.keymap.set("n", "<C-/>", ":CommentToggle")
+vim.keymap.set("v", "<C-/>", ":CommentToggle")
+
+vim.keymap.set("n", "<leader>rn", ":IncRename ")
