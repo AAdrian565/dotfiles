f830d50537767ee835c24046d85c96169934fcbf
= <adrianagustinust@gmail.com>
2024-02-05 16:44:57 +0700

    telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,13 +1,28 @@
+local file_ignore_patterns = {
+    "./node_modules/*",
+    "node_modules",
+    "^node_modules/*",
+    "node_modules/*",
+    "^%.cache/*",
+    ".git",
+    ".*%.o",
+    "*/.bin",
+}
+
+require("telescope").setup({
+    defaults = {
+        file_ignore_patterns = file_ignore_patterns,
+        mappings = {
+            n = {
+                ["g?"] = "which_key",
+            },
+        },
+    },
+})
+
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>pf", ":Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>", {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    file_ignore_patterns = {
-        "./node_modules/*",
-        "node_modules",
-        "^node_modules/*",
-        "node_modules/*",
-        "*/.bin",
-    }
+    builtin.grep_string({ search = vim.fn.input("Grep > "), file_ignore_patterns = file_ignore_patterns })
 end)
